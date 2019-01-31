d642ebc83457779f534a88b5b3d394cce190daa7
Andre Santos <asantos@kiman.com.br>
2019-01-31 15:38:35 -0200

    First Commit

---
+++ src/main/java/com/webmall/product/WebmallProductApplication.java
@@ -0,0 +1,14 @@
+package com.webmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmallProductApplication.class, args);
+	}
+
+}
+
